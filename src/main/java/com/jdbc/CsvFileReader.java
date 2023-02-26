10e6a1c5274a80401f43fd50f386d6d7c64b8cf4
usha-12 <usha12ahirwar@gmail.com>
2023-02-26 16:50:44 +0530

    Added: How to Read data from csv file on our console

--- src/main/java/com/jdbc/CsvFileReader.java
+++ src/main/java/com/jdbc/CsvFileReader.java
@@ -1,2 +1,28 @@
-package com.jdbc;public class CsvFileReader {
+package com.jdbc;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class CsvFileReader {
+    public static void main(String[] args) throws IOException, CsvException {
+        FileReader filereader = new FileReader("C:\\Users\\User\\Desktop\\fileReader.csv");
+        // create csvReader object and skip first Line
+        CSVReader csvReader = new CSVReaderBuilder(filereader).withSkipLines(1).build();
+        List<String[]> allData = csvReader.readAll();
+
+        // print Data
+        for (String[] row : allData) {
+            for (String cell : row) {
+                System.out.print(cell + "\t");
+            }
+            System.out.println();
+        }
+
+    }
 }
