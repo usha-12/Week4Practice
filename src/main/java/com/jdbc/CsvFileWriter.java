ec8cd91b97fcf3b79b47aebb7e28d4958e99edb5
usha-12 <usha12ahirwar@gmail.com>
2023-02-26 16:51:40 +0530

    Added: how to write data on our csv file

--- src/main/java/com/jdbc/CsvFileWriter.java
+++ src/main/java/com/jdbc/CsvFileWriter.java
@@ -1,5 +1,26 @@
 package com.jdbc;
 
-public class CsvFileWriter {
+import com.opencsv.CSVWriter;
 
-}
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvFileWriter {
+    public static void main(String[] args) throws IOException {
+        FileWriter outPutFile = new FileWriter("C:\\Users\\User\\Desktop\\FileWritter.csv");
+        // create CSVWriter object fileWriter object as parameter
+        CSVWriter writer = new CSVWriter(outPutFile);
+        // create a List which contains String array
+        List<String[]> data = new ArrayList<String[]>();
+        data.add(new String[] { "Name", "Class", "Marks" });
+        data.add(new String[] { "Aman", "10", "620" });
+        data.add(new String[] { "Suraj", "10", "630" });
+        writer.writeAll(data);
+
+     // closing writer connection
+        writer.close();
+    }
+    }
+
